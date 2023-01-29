fcbd7885bb034fb98e41905667290a78a64778dc
happyyumeno <happythousandsun@gmail.com>
2023-01-29 20:27:13 +0900

    1st commit

--- main.py
+++ main.py
@@ -4,36 +4,36 @@ st.title('もち')
 
 button=st.button('もちの写真')
 
-# if button:
-#     # img=Image.open('moti2.JPG')
-#     # st.image(img,caption='もち',use_column_width=True)
+if button:
+    # img=Image.open('moti2.JPG')
+    # st.image(img,caption='もち',use_column_width=True)
 
-#     col1, col2, col3 = st.columns(3)
+    col1, col2, col3 = st.columns(3)
 
-#     ex=st.expander('ex')
-#     ex.write("A")
-#     ex.image("https://github.com/happyyumeno/moti/blob/master/moti.JPG?raw=true",use_column_width=True)
+    ex=st.expander('ex')
+    ex.write("A")
+    ex.image("https://github.com/happyyumeno/moti/blob/master/moti.JPG?raw=true",use_column_width=True)
     
 
-#     with col1:
-#         st.header("A")
-#         st.image("https://github.com/happyyumeno/moti/blob/master/moti.JPG?raw=true", use_column_width=True)
+    with col1:
+        st.header("A")
+        st.image("https://github.com/happyyumeno/moti/blob/master/moti.JPG?raw=true", use_column_width=True)
         
 
-#     with col2:
-#         st.header("B")
-#         st.image("https://github.com/happyyumeno/moti/blob/master/moti2.JPG?raw=true", use_column_width=True)
+    with col2:
+        st.header("B")
+        st.image("https://github.com/happyyumeno/moti/blob/master/moti2.JPG?raw=true", use_column_width=True)
     
-#     with col3:
-#         st.header("C")
-#         st.image("https://github.com/happyyumeno/moti/blob/master/moti3.jpg?raw=true", use_column_width=True)
+    with col3:
+        st.header("C")
+        st.image("https://github.com/happyyumeno/moti/blob/master/moti3.jpg?raw=true", use_column_width=True)
 
-# button=st.button('とじる')
-# # if button:
-#     # img=Image.close('')
+button=st.button('とじる')
+# if button:
+    # img=Image.close('')
 
-# """
-# # もちかわいい
-# ## もち可愛い
-# ### もちすごい可愛いよね？
-# """
+"""
+# もちかわいい
+## もち可愛い
+### もちすごい可愛いよね？
+"""
