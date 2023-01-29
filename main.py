0b20d74d78b81087e7dc702acae463358e4eccb3
happyyumeno <happythousandsun@gmail.com>
2023-01-29 14:23:05 +0900

    1st commit

---
+++ main.py
@@ -0,0 +1,42 @@
+import streamlit as st
+from PIL import Image
+
+st.title('もち')
+
+
+
+button=st.button('もちの写真')
+
+if button:
+    # img=Image.open('moti2.JPG')
+    # st.image(img,caption='もち',use_column_width=True)
+
+    col1, col2, col3 = st.columns(3)
+
+    ex=st.expander('ex')
+    ex.write("A")
+    ex.image("moti.JPG",use_column_width=True)
+    
+
+    with col1:
+        st.header("A")
+        st.image("moti.JPG", use_column_width=True)
+        
+
+    with col2:
+        st.header("B")
+        st.image("moti2.JPG", use_column_width=True)
+    
+    with col3:
+        st.header("C")
+        st.image("moti3.JPG", use_column_width=True)
+
+button=st.button('とじる')
+if button:
+    img=Image.close('')
+
+"""
+# もちかわいい
+## もち可愛い
+### もちすごい可愛いよね？
+"""
